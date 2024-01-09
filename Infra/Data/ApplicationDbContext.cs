1dab6256accc70c92526e899adc068cd028e9840
Mauro Alves <mauro.alves.aguiar@outlook.com>
2024-01-08 23:05:47 -0300

    Commit para upload de arquivos do projeto

---
+++ Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using IWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace IWantApp.Infra.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>().Property(p => p.Description).IsRequired(false);
+        builder.Entity<Product>().Property(p => p.Name).HasMaxLength(255).IsRequired();
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configuration)
+    {
+        
+        modelBuilder.Entity<Product>().Property(p => p.Code).HasColumnType("decimal(18,2)").IsRequired();
+        modelBuilder.Entity<Category>().ToTable("Categories");
+    }
+}
